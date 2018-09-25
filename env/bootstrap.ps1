c97e758fce2fd1f4f557823cbe54b92715463da7
xiaodoli <xiaodoli@microsoft.com>
2018-09-25 13:29:02 -0700

    need to use the same network

--- env/bootstrap.ps1
+++ env/bootstrap.ps1
@@ -7,7 +7,8 @@ Write-Host "Deploy influxdb..."
 Remove-Item -Recurse -Force C:\influxdb -ErrorAction SilentlyContinue
 New-Item -Path c:\influxdb -ItemType Directory | Out-Null
 Copy-Item $envFolder\influxdb.conf -Destination c:\influxdb 
-docker run -d --name influxdb -p 8086:8086 `
+docker network create influxdb
+docker run -d --name influxdb --net=influxdb -p 8086:8086 `
     -v c:\influxdb\influxdb.conf:/etc/influxdb/influxdb.conf:ro `
     -v c:\influxdb:/var/lib/influxdb:rw `
     influxdb -config /etc/influxdb/influxdb.conf
@@ -16,11 +17,11 @@ Write-Host "run telegraf..."
 Remove-Item -Recurse -Force c:\telegraf -ErrorAction SilentlyContinue
 New-Item -Path c:\telegraf -ItemType Directory | Out-Null
 Copy-Item -Path $envFolder\telegraf.conf -Destination C:\telegraf\telegraf.conf 
-docker run -d --name telegraf -v C:\telegraf\telegraf.conf:/etc/telegraf/telegraf.conf:ro telegraf 
+docker run -d --name telegraf --net=influxdb -v C:\telegraf\telegraf.conf:/etc/telegraf/telegraf.conf:ro telegraf 
 
-docker run -d --name chronograf -p 8888:8888 --net=container:influxdb chronograf --influxdb-url=http://localhost:8086
+docker run -d --name chronograf -p 8888:8888 --net=influxdb chronograf --influxdb-url=http://localhost:8086
 
-docker run -d --name=grafana -p 3000:3000 --net=container:influxdb grafana/grafana
+docker run -d --name=grafana -p 3000:3000 --net=influxdb grafana/grafana
 
 New-Item -Path c:\fluentd -ItemType Directory | Out-Null
 docker run -d --name fluentd -p 24224:24224 -p 24224:24224/udp -v c:\fluentd:/fluentd/log:rw fluent/fluentd
